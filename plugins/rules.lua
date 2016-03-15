a7f93d508026fd68c6e61a521e57ea2f7c273638
bamdad1381 <bamdadnikkhah@gmail.com>
2016-03-15 11:36:05 +0330

    Update rules.lua

--- plugins/rules.lua
+++ plugins/rules.lua
@@ -1,25 +1,14 @@
---------------------------------------------------
---      ____  ____ _____                        --
---     |    \|  _ )_   _|___ ____   __  __      --
---     | |_  )  _ \ | |/ ·__|  _ \_|  \/  |     --
---     |____/|____/ |_|\____/\_____|_/\/\_|     --
---                                              --
---------------------------------------------------
---                                              --
---       Developers: @Josepdal & @MaSkAoS       --
---     Support: @Skneos,  @iicc1 & @serx666     --
---                                              --
---------------------------------------------------
+
 
 do
 
 function run(msg, matches)
   return [[
-ℹ️ Rules:
-1⃣ No Flood.
-2⃣ No Spam.
-3⃣ Try to stay on topic.
-4⃣ Forbidden any racist, sexual, homophobic or gore content.
+✴ Rules:
+❕ No Flood.
+❕ No Spam.
+❕ Try to stay on topic.
+❕ Forbidden any racist, sexual, homophobic or gore content.
 ➡️ Repeated failure to comply with these rules will cause ban.]]
 end
 
